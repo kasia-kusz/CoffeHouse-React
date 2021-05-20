7659675d26f987a48b30fc67b5e5f482f39cfed8
kasia-kusz <katarzyna.kuszx@gmail.com>
2021-05-20 12:20:14 +0200

    Assets

--- coffeehouse-react/src/components/Showcase/Showcase.js
+++ coffeehouse-react/src/components/Showcase/Showcase.js
@@ -3,7 +3,19 @@ import "./styles.js";
 import { Container } from "./styles.js";
 
 const Showcase = () => {
-  return <Container className="showcase">Showcase</Container>;
+  return (
+    <Container className="showcase">
+      <div className="showcase main-showcase">
+        <div className="logo">
+          <img
+            className="logo-img"
+            src="../../assets/coffee-logo-svg.svg"
+            alt="Coffee logo"
+          />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Showcase;
