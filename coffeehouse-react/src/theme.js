6c4b63c3171f62af521acb30468d8f0514ae8899
kasia-kusz <katarzyna.kuszx@gmail.com>
2021-05-20 11:31:05 +0200

    Theme colors and breakpoints

--- coffeehouse-react/src/theme.js
+++ coffeehouse-react/src/theme.js
@@ -2,9 +2,23 @@ const theme = {
   colors: {
     black: "#000",
     white: "#fff",
+    darkColor: "#333",
+    primaryColor: "#d4af37",
+    secondaryColor: "#6e5710",
+    brokenWhite: "#eee",
+    lightWhite: "rgb(250, 250, 250)",
+    text: "#e1ecf5",
   },
   breakpoints: {
+    laptop_big: "1601px",
+    laptop: "1480px",
+    laptop_small: "1281px",
+    tablet_big: "1100px",
     tablet: "867px",
+    tablet_small: "769px",
+    phone_big: "601px",
+    phone: "431px",
+    phone_small: "376px",
   },
 };
 
